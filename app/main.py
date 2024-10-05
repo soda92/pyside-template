c26c277a5feebf200be76c968c86fe884251a9f6
Maple <wang-jiaxun@outlook.com>
2024-10-05 20:22:28 +0800

    add translation

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ import os
 import platform
 import sys
 import widgets.mainwindow as mainwindow
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore
+import main_rc  # noqa: F401
 
 
 def main_gui():
@@ -12,6 +13,10 @@ def main_gui():
             os.environ.get("QT_PLUGIN_PATH", "") + ";" + "/usr/lib/qt6/plugins/"
         )
     app = QtWidgets.QApplication(sys.argv)
+
+    translator = QtCore.QTranslator()
+    translator.load(":/translation_zh_CN.qm")
+    app.installTranslator(translator)
 
     _window = mainwindow.MainWindow()
     _window.show()
---
+++ translation_zh_CN.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="form/mainwindow.ui" line="14"/>
+        <source>Form</source>
+        <translation>表单</translation>
+    </message>
+    <message>
+        <location filename="form/mainwindow.ui" line="26"/>
+        <source>Hello, world!</source>
+        <translation>你好，世界！</translation>
+    </message>
+</context>
+</TS>
